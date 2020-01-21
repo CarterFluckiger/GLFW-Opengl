c2dd19fce545e934cc1785f149d1c9f22f28e582
Carter Fluckiger <fluckiger2@tritions.iowacentral.edu>
2020-01-20 21:28:53 -0600

    Initial Commit

---
+++ GLFW Opengl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GLFW Opengl
+//
+//  Created by Tag1 on 1/20/20.
+//  Copyright © 2020 Carter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
